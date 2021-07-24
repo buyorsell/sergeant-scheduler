419b7931b395b209b8a63c8ef6a056251930c50c
fodro <feodor.tomilov@yandex.ru>
2021-07-24 14:34:37 +0300

    update schedule

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import warnings
 import os, requests, schedule, time, threading, logging, sys
 from datetime import datetime
 from plots.fin_plot import upd_secs_plots
+from ml.news_flow import update_flow
 moex_host = os.environ.get('MOEX_HOST')
 news_host = os.environ.get('NEWS_HOST')
 
@@ -15,18 +16,30 @@ warnings.warn = warn
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 def kick_moex():
-    logging.info("Kicking moex.....")
+    logging.info("Kicking moex.....?")
     weekday = datetime.today().weekday()
     if weekday == 4:
+        logging.info("Yep, it's time!")
         today = datetime.now().date()
         #yesterday = today - datetime.timedelta(days=1)
         requests.get(moex_host + "/" + str(today))
         upd_secs_plots()
+    return
 
 
 def kick_news():
     logging.info("Kicking news.......")
     requests.get(news_host + "/")
+    return
+
+
+def kick_flow():
+    logging.info("Kicking flow.......?")
+    weekday = datetime.today().weekday()
+    if weekday == 6:
+        logging.info("Yep, it's time!")
+        update_flow()
+    return
 
 
 def run_threaded(job_func):
@@ -35,6 +48,7 @@ def run_threaded(job_func):
 
 
 schedule.every().day.at("21:00").do(run_threaded, kick_moex)
+schedule.every().day.at("21:00").do(run_threaded, kick_flow)
 schedule.every(30).minutes.do(run_threaded, kick_news)
 
 while True:
